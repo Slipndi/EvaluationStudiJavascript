b827adfd819c52674129b3bfe1cfc607b15bf814
Ludovic JORGE DO MARCO <48907643+Slipndi@users.noreply.github.com>
2022-02-20 15:06:32 +0100

    Update of colors

---
+++ tailwind.config.js
@@ -0,0 +1,12 @@
+const colors = require('tailwindcss/colors'); 
+
+module.exports = {
+  content: ["**.html"],
+  theme: {
+    colors: {
+      primary: colors.red,
+      secondary: colors.orange
+    }
+  },
+  plugins: [],
+}
